5e5a087ccdeef5bb76da7abbed059b1b6d8f71d4
Motiur Rahman <codehiker07@gmail.com>
2024-04-29 21:56:35 +0600

    image link added

--- flipKart-scraping.mjs
+++ flipKart-scraping.mjs
@@ -2,7 +2,7 @@ import puppeteer from "puppeteer";
 import fs from 'fs';
 
 async function scraper() {
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     await page.goto('https://www.flipkart.com/search?q=mobile&otracker=search&otracker1=search&marketplace=FLIPKART&as-show=off&as=off', {
         timeout: 0,
@@ -20,12 +20,14 @@ async function scraper() {
             const name = item.querySelector('.KzDlHZ') && item.querySelector('.KzDlHZ').innerText
             const rating = item.querySelector('.Y1HWO0') && item.querySelector('.Y1HWO0').innerText
             const description = item.querySelector('._6NESgJ') && item.querySelector('._6NESgJ').innerText
+            const image = item.querySelector('._4WELSP .DByuf4').src
 
             data.list.push({
                 name: name,
                 id: id,
                 rating: rating,
                 description: description,
+                image: image,
             })
 
         })
